3124c97185e809257ba222f6d41a0f6df09c540a
shozo hatta <hachi8833@gmail.com>
2014-01-15 22:51:59 +0900

    オプションgemをコメントアウト

--- app_template.rb
+++ app_template.rb
@@ -18,210 +18,106 @@ source 'https://rubygems.org'
 
 ruby '2.1.0'
 
-## 共通
-# 最新のrails
-gem 'rails', '4.0.2'
-
-# devise: ユーザー認証gem
-gem 'devise'
-
-# mysql コネクタgem
-gem 'mysql2'
-
-# SASS スタイルシート導入
-gem 'sass'
-gem 'sass-rails'
-
-# LESSにしたい人はこちら
-#gem 'less-rails'
-
-# Uglifier: JavaScriptを圧縮&難読化
-gem 'uglifier', '>= 1.3.0'
-
-# CoffeeScript導入
-gem 'coffee-rails', '~> 4.0.0'
-
-# therubyracerはV8 JavaScriptエンジンをrubyから使うためのgemだが、コンパイルが始まるとインストールに時間がかかるので外してある。参照: http://d.hatena.ne.jp/suu-g/20121222/1356189597
-# gem 'therubyracer', platforms: :ruby
-
-gem 'jquery-rails'
-# Jquery導入
-
-# Turbolinks導入。邪魔だが依存関係があるので入れてある
-gem 'turbolinks'
-
-# JSON APIをビルドする。Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 1.2'
-
-group :doc do
-  # APIドキュメントを生成するには: bundle exec rake doc:rails generates the API under doc/api
-  gem 'sdoc', require: false
-end
-
-# アセットのログを削除
-gem 'quiet_assets'
-
-# HTML5 validation
-gem 'html5_validators'
-
-# PG/MySQL ログフォーマッタ
-gem 'rails-flog'
-
-# 秘密鍵などをENV経由で利用する https://github.com/laserlemon/figaro
-# gem 'figaro'
-
-# ActiveModel でパスワード暗号化に使用。http://bakunyo.hatenablog.com/entry/2013/05/26/bcrypt-ruby%E3%82%92Rails%E3%81%A7%E4%BD%BF%E3%81%86
-  gem 'bcrypt-ruby', '~> 3.1.2'
-
-# デプロイ自動化 http://labs.gree.jp/blog/2013/12/10084/
-  gem 'capistrano', group: :development
-
-# エラー表示はbetter_errors一択
-# gem 'debugger', group: [:development, :test]
-  gem 'better_errors'
-
-# Bower Manager => https://rails-assets.org/
-source 'https://rails-assets.org'
-
-# jqueryでのturbolinksサポート
-gem 'jquery-turbolinks'
-
-# SourceMaps (圧縮されたJavaScriptのバグをブラウザ上でトラックする) http://www.publickey1.jp/blog/12/javascriptcoffeescriptsource_maps.html
-gem 'coffee-rails-source-maps'
-gem 'sass-rails-source-maps'
-
-# プレゼンター層の追加
-gem 'draper'
-
-# Hamlの導入
+# regular gems http://www.ohmyenter.com/?p=371
+gem 'rails', '~> 4.0.2'
 gem 'haml-rails'
+gem 'mysql2'
+gem 'sass-rails'
+gem 'uglifier'   #JavaScriptの圧縮&難読化
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jquery-turbolinks' # jqueryでのturbolinksサポート
+gem 'jbuilder'   # JSON APIをビルドする。Read more: https://github.com/rails/jbuilder
 
-# フォーム作成支援 http://www.ohmyenter.com/?p=197
-gem 'simple_form'
-
-# herokuでプロセス管理する場合に使用 http://www.ownway.info/Ruby/index.php?foreman/about
-# gem 'foreman'
-
-# ページ切り替え処理
-# gem 'kaminari' http://memo.yomukaku.net/entries/238
-
-# パフォーマンス管理・サービス監視 NewRelic https://github.com/newrelic/rpm
-gem 'newrelic_rpm'
-
-# エラー時にメール通知 http://www.slideshare.net/morimorihoge1/gem-24158289
-# gem 'airbrake'
-
-# HTML/XMLパーサー http://nokogiri.org/
-# gem 'nokogiri'
-
-# 重たい処理を非同期で実行 http://blog.nzm-o.com/item/222
-# gem 'delayed_job'
-
-# cron処理 http://morizyun.github.io/blog/whenever-gem-rails-ruby-capistrano/
-# gem 'whenever'
-
-# 添付ファイルをActiveRecord透過で保存できる (ImageMagick必要) https://github.com/thoughtbot/paperclip
-# gem 'paperclip'
-
-# Rails/Rack Profiler
-# gem 'speed_gun'
-
-# Hash extensions
-# gem 'hashie'
-
-# PDFサポート。ただしMIMEタイプがらみでwarningが出る。
-gem 'prawn-rails'
-
-# refactoring suggestions
-gem 'rails_best_practices'
-
-# doorkeeper: OAuth2 authentication
-# gem 'doorkeeper'
-
-#------- reserverd ---------#
-# friently_id: changes URL to non-REST string form
-# gem 'friendly_id'
-
-# ransack: search form
-# gem 'ransack'
+gem 'draper' #Presenter層の追加
+gem 'ransack' #簡易検索機能
+gem 'simple_form' # フォーム作成支援 http://www.ohmyenter.com/?p=197
 
 group :development do
-  # Converter erb => haml
-  gem 'erb2haml'
-
-  # view/cssの更新監視・ブラウザリロード
-  gem 'guard-livereload'
-  gem 'em-websocket'
-
+  gem 'rack-mini-profiler'
 end
 
 group :development, :test do
-
-  # ローカルweb server
-  gem 'thin'
-
-  # Rails application preloader
+  gem 'thin' # ローカルweb server
+  gem 'better_errors'
+  gem 'pry-rails'
+  gem 'pry-doc'
+  gem 'pry-byebug'
+  gem 'pry-coolline' #Pry入力色付け
+  gem 'awesome_print' #Pry色付け
+  gem 'hirb' #pryのSQLを整形
+  gem 'hirb-unicode'
+  gem 'rspec-rails'
   gem 'spring'
   gem 'spring-commands-rspec'
+end
 
-  # Railsコンソールの多機能版
-  gem 'pry-rails'
-
-  # pryの入力に色付け
-  gem 'pry-coolline'
-
-  # pryの色付けをしてくれる
-  gem 'awesome_print'
-
-  # デバッカー
-  gem 'pry-byebug'
-
-  # Pryでの便利コマンド
-  gem 'pry-doc'
-
-  # PryでのSQLの結果を綺麗に表示
-  gem 'hirb'
-  gem 'hirb-unicode'
-
-  # coverage
-  gem "simplecov", require: false
-
-  # Rspec
-  gem 'rspec-rails'
-  gem 'rake_shared_context'
-  gem 'capybara'
-
-  # fixtureの代わり
-  gem "factory_girl_rails"
-  gem 'faker'
+group :test do
+  gem "shoulda-matchers"
+  gem 'factory_girl_rails'
+  gem 'launchy' #save_and_open_pageをspecに追加するとブラウザを表示する
+  gem 'capybara' #ブラウザのテストに使う
+  gem 'faker' #それっぽいテストデータを生成する
   gem 'faker-japanese'
+  gem 'poltergeist' #phantomjsをインストールする必要あり
+  gem "simplecov", require: false #カバレッジ
+  gem 'database_rewinder'   # テスト環境のテーブルをきれいにする
+end
 
-  # HTTP requestのモックアップ作成 http://d.hatena.ne.jp/kitamomonga/20100325/ruby_hello_webmock
-  gem 'webmock'
-
-  # 追加マッチャー集 http://morizyun.github.io/blog/shoulda-matchers-rspec-matcher/
-  gem 'shoulda-matchers'
-
-  # テスト環境のテーブルをきれいにする
-  gem 'database_rewinder'
-
-  # Guard
-  gem 'guard'
-  gem 'guard-rspec'
-  gem 'guard-spring'
-
-  # Gemfileが更新されたら自動でbundle installを実行
-  gem 'guard-bundler'
-
+group :doc do
+  gem 'sdoc', require: false
 end
 
 group :production, :staging do
-  # ログ保存先変更、静的アセット Heroku 向けに調整
-  gem 'rails_12factor'
-
-  # Use unicorn as the app server
-  gem 'unicorn'
+  gem 'unicorn' # 本番用Webサーバー
+  # gem 'rails_12factor'   # ログ保存先変更、静的アセット Heroku 向けに調整
 end
+
+# オプション ############
+# gem 'devise' #ユーザー認証
+# gem 'sqlite3' #
+# gem 'therubyracer', platforms: :ruby
+# gem 'quiet_assets' # アセットのログを削除
+# gem 'html5_validators' # フォーム入力の自動バリデーションhttps://github.com/amatsuda/html5_validators
+# gem 'rails-flog' # PG/MySQL ログフォーマッタ
+# gem 'figaro' # 秘密鍵などをENV経由で利用する https://github.com/laserlemon/figaro
+# gem 'bcrypt-ruby', '~> 3.1.2' # ActiveModel でパスワード暗号化に使用。http://bakunyo.hatenablog.com/entry/2013/05/26/bcrypt-ruby%E3%82%92Rails%E3%81%A7%E4%BD%BF%E3%81%86
+# gem 'capistrano', group: :development # デプロイ自動化 http://labs.gree.jp/blog/2013/12/10084/
+# SourceMaps (圧縮されたJavaScriptのバグをブラウザ上でトラックする) http://www.publickey1.jp/blog/12/javascriptcoffeescriptsource_maps.html
+  # gem 'coffee-rails-source-maps'
+  # gem 'sass-rails-source-maps'
+# gem 'foreman' # herokuでプロセス管理する場合に使用 http://www.ownway.info/Ruby/index.php?foreman/about
+# gem 'kaminari' # ページ切り替え処理 http://memo.yomukaku.net/entries/238
+# gem 'newrelic_rpm' # パフォーマンス管理・サービス監視 NewRelic https://github.com/newrelic/rpm
+# gem 'airbrake' # エラー時にメール通知 http://www.slideshare.net/morimorihoge1/gem-24158289
+# gem 'nokogiri' # HTML/XMLパーサー http://nokogiri.org/
+# gem 'delayed_job' # 重たい処理を非同期で実行 http://blog.nzm-o.com/item/222
+# gem 'whenever' # cron処理 http://morizyun.github.io/blog/whenever-gem-rails-ruby-capistrano/
+# gem 'paperclip' # 添付ファイルをActiveRecord透過で保存できる (ImageMagick必要) https://github.com/thoughtbot/paperclip
+# gem 'speed_gun' #パフォーマンス測定 http://rosylilly.hatenablog.com/entry/2013/12/03/184748
+# gem 'hashie' # Hashの拡張
+# gem 'prawn-rails' # PDFサポート。ただしMIMEタイプがらみでwarningが出る。
+# gem 'rails_best_practices' # リファクタリングのアシスタント
+# gem 'doorkeeper' #OAuth2認証
+# gem 'friendly_id' #URLをクエリ形式にする
+
+group :development do
+  gem 'erb2haml' # Converter erb => haml
+  # view/cssの更新監視・ブラウザリロード
+  # gem 'guard-livereload'
+  # gem 'em-websocket'
+end
+
+group :development, :test do
+  # gem 'rake_shared_context'
+  # gem 'webmock'   # HTTP requestのモックアップ作成 http://d.hatena.ne.jp/kitamomonga/20100325/ruby_hello_webmock
+  # gem 'guard' #ファイル更新をウオッチ
+  # gem 'guard-rspec'
+  # gem 'guard-spring'
+  # gem 'guard-bundler' # Gemfileが更新されたら自動でbundle installを実行
+end
+
 CODE
 
 # install gems
@@ -251,7 +147,7 @@ application  do
       g.helper_specs false
       g.request_specs false
       g.feature_specs true
-      g.decorator_specs false
+      g.decorator_specs true
       g.assets false
       g.helper false
     end
@@ -289,24 +185,24 @@ insert_into_file 'app/views/layouts/application.html.haml',%(
 generate 'simple_form:install --bootstrap'
 
 # Kaminari config
-generate 'kaminari:config'
-generate 'kaminari:views  bootstrap'
+# generate 'kaminari:config'
+# generate 'kaminari:views  bootstrap'
 
-run 'rm -rf app/view/kaminari/_paginator.html.haml'
-file 'app/view/kaminari/_paginator.html.haml', <<-FILE
-= paginator.render do
-  .pagination{style: 'text-align: center; display: block;'}
-    %ul.pagination
-      = first_page_tag unless current_page.first?
-      = prev_page_tag unless current_page.first?
-      - each_page do |page|
-        - if page.left_outer? || page.right_outer? || page.inside_window?
-          = page_tag page
-        - elsif !page.was_truncated?
-          = gap_tag
-      = next_page_tag unless current_page.last?
-      = last_page_tag unless current_page.last?
-FILE
+# run 'rm -rf app/view/kaminari/_paginator.html.haml'
+# file 'app/view/kaminari/_paginator.html.haml', <<-FILE
+# = paginator.render do
+#   .pagination{style: 'text-align: center; display: block;'}
+#     %ul.pagination
+#       = first_page_tag unless current_page.first?
+#       = prev_page_tag unless current_page.first?
+#       - each_page do |page|
+#         - if page.left_outer? || page.right_outer? || page.inside_window?
+#           = page_tag page
+#         - elsif !page.was_truncated?
+#           = gap_tag
+#       = next_page_tag unless current_page.last?
+#       = last_page_tag unless current_page.last?
+# FILE
 
 # Database
 run 'rm -rf config/database.yml'
@@ -346,18 +242,14 @@ insert_into_file 'spec/spec_helper.rb', "\nrequire 'factory_girl_rails'", "\nreq
 gsub_file 'spec/spec_helper.rb', "require 'rspec/autorun'", ''
 
 # Spring
-run 'wget https://raw.github.com/jonleighton/spring/master/bin/spring -P bin/'
-run 'sudo chmod a+x bin/spring'
+# run 'wget https://raw.github.com/jonleighton/spring/master/bin/spring -P bin/'
+# run 'sudo chmod a+x bin/spring'
 
 # Guard
-run 'bundle exec guard init'
-gsub_file 'Guardfile', 'guard :rspec do', "guard :rspec, cmd: 'bundle exec spring rspec -f doc' do"
+# run 'bundle exec guard init'
+# gsub_file 'Guardfile', 'guard :rspec do', "guard :rspec, cmd: 'bundle exec spring rspec -f doc' do"
 
 run 'bundle install'
 
 # git flow init
-# ----------------------------------------------------------------
 run 'git flow init'
-# git :init
-git :add => '.'
-git :commit => "-a -m 'first commit'"
