4c917c7bcefe78df9e1be26a068dc521df825c46
shozo hatta <hachi8833@gmail.com>
2014-02-02 20:03:26 +0900

    タイポ修正

--- app_template.rb
+++ app_template.rb
@@ -274,18 +274,19 @@ insert_into_file 'config/environments/development.rb',%(
   Rack::MiniProfiler.config.position = 'right'
 
   config.after_initialize do
-  Bullet.enable = true
-  Bullet.alert = true
-  Bullet.bullet_logger = true
-  Bullet.console = true
-  Bullet.growl = true
-  Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
-                  :password => 'bullets_password_for_jabber',
-                  :receiver => 'your_account@jabber.org',
-                  :show_online_status => true }
-  Bullet.rails_logger = true
-  Bullet.airbrake = true
-  Bullet.add_footer = true
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.growl = true
+    Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
+                    :password => 'bullets_password_for_jabber',
+                    :receiver => 'your_account@jabber.org',
+                    :show_online_status => true }
+    Bullet.rails_logger = true
+    Bullet.airbrake = true
+    Bullet.add_footer = true
+  end
 ), after: 'config.assets.debug = true'
 
 gsub_file 'app/views/layouts/application.html.haml', /= yield/, ''
