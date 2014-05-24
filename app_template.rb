65df599fdfe33276ef2205c0dc266fb78ec09c01
shozo hatta <hachi8833@gmail.com>
2014-05-24 15:55:18 +0900

    nokogiriは別途ビルド&インストールすることに

--- app_template.rb
+++ app_template.rb
@@ -32,7 +32,6 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jquery-turbolinks' # jqueryでのturbolinksサポート
 gem 'jbuilder'   # JSON APIをビルドする。Read more: https://github.com/rails/jbuilder
-gem 'nokogiri' --with-xml2-include=/usr/local/opt/libxml2/include/libxml2 # HTML/XMLパーサー http://nokogiri.org/
 
 gem 'bootstrap-sass'
 gem 'font-awesome-rails'
@@ -113,6 +112,7 @@ end
 # gem 'bcrypt', '~> 3.1.7' # ActiveModel でパスワード暗号化に使用。http://bakunyo.hatenablog.com/entry/2013/05/26/bcrypt-ruby%E3%82%92Rails%E3%81%A7%E4%BD%BF%E3%81%86
 # gem 'capistrano-rails', group: :development # デプロイ自動化 http://labs.gree.jp/blog/2013/12/10084/
 # SourceMaps (圧縮されたJavaScriptのバグをブラウザ上でトラックする) http://www.publickey1.jp/blog/12/javascriptcoffeescriptsource_maps.html
+# gem 'nokogiri'# HTML/XMLパーサー http://nokogiri.org/
   # gem 'coffee-rails-source-maps'
   # gem 'sass-rails-source-maps'
 # gem 'foreman' # herokuでプロセス管理する場合に使用 http://www.ownway.info/Ruby/index.php?foreman/about
